d109ab84b5036ebb66f23d2eddc5edf3e801de23
Robert Andersson <inputfalken@gmail.com>
2016-12-27 23:51:21 +0100

    First commit

---
+++ Monads/Lazy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Monads {
+    internal static class Lazy {
+        public static Lazy<T> ToLazy<T>(this T t) => new Lazy<T>(() => t);
+
+        public static Lazy<TResult> Select<T, TResult>(this Lazy<T> source, Func<T, TResult> func) => new Lazy<TResult>(
+            () => func(source.Value));
+
+        public static Lazy<TResult> SelectMany<T, TResult>(this Lazy<T> source,
+            Func<T, Lazy<TResult>> func) => new Lazy<TResult>(() => func(source.Value).Value);
+
+        public static Lazy<TResult> SelectMany<T, T2, TResult>(this Lazy<T> source, Func<T, Lazy<T2>> func,
+            Func<T, T2, TResult> func2) => new Lazy<TResult>(() => func2(source.Value, func(source.Value).Value));
+    }
+}
---
+++ Monads/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Monads {
+    internal static class Program {
+        public static void Main(string[] args) {
+            var bind = 20.ToLazy().SelectMany(i => 40.ToLazy(), (i, i1) => i + i1);
+            var lazyString = "Hello".ToLazy().SelectMany(s => (s + " " + "World").ToLazy());
+
+            var foo = from text in lazyString
+                from number in bind
+                select text + number;
+        }
+    }
+
+}
