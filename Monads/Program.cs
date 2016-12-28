752fc1e445ebc7b881322f6398016cca360494b9
Robert Andersson <inputfalken@gmail.com>
2016-12-28 01:39:01 +0100

    Changes variable names to be more suiting

--- Monads/Program.cs
+++ Monads/Program.cs
@@ -1,13 +1,16 @@
-﻿namespace Monads {
+﻿using System;
+
+namespace Monads {
     internal static class Program {
         public static void Main(string[] args) {
-            var bind = 20.ToLazy().SelectMany(i => 40.ToLazy(), (i, i1) => i + i1);
+            var lazyInteger = 20.ToLazy().SelectMany(i => 40.ToLazy(), (i, i1) => i + i1);
             var lazyString = "Hello".ToLazy().SelectMany(s => (s + " " + "World").ToLazy());
 
-            var foo = from text in lazyString
-                from number in bind
+            var lazyConcatenation = from text in lazyString
+                from number in lazyInteger
                 select text + number;
+
+            Console.WriteLine(lazyConcatenation.Value);
         }
     }
-
 }
