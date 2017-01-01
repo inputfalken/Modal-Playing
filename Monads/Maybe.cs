4575513b4812597983e1feea32d1ff1344c5280f
Robert Andersson <inputfalken@gmail.com>
2017-01-01 22:41:56 +0100

    Reorders methods

--- Monads/Maybe.cs
+++ Monads/Maybe.cs
@@ -27,9 +27,6 @@ namespace Monads {
     public static class Maybe {
         public static Maybe<T> ToMaybe<T>(this T t) => new Maybe<T>(t);
 
-        public static Maybe<TResult> Select<T, TResult>(this Maybe<T> maybe, Func<T, TResult> func)
-            => SelectMany(maybe, arg => ToMaybe(func(arg)));
-
         public static Maybe<TResult> SelectMany<T, TResult>(this Maybe<T> maybe,
             Func<T, Maybe<TResult>> func) => maybe.HasValue
             ? func(maybe.Value)
@@ -37,6 +34,9 @@ namespace Monads {
 
         public static Maybe<TResult> SelectMany<T, T2, TResult>(this Maybe<T> maybe, Func<T, Maybe<T2>> func,
             Func<T, T2, TResult> func2) => Select(SelectMany(maybe, func), arg => func2(maybe.Value, arg));
+
+        public static Maybe<TResult> Select<T, TResult>(this Maybe<T> maybe, Func<T, TResult> func)
+            => SelectMany(maybe, arg => ToMaybe(func(arg)));
 
         public static Maybe<TResult> Where<TResult>(this Maybe<TResult> maybe, Func<TResult, bool> predicate)
             => SelectMany(maybe, x => predicate(x) ? maybe : Maybe<TResult>.Nothing);
