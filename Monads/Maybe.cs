97ef37ef3eb57fb518fee62be6b4e37bb76eb341
Robert Andersson <inputfalken@gmail.com>
2017-01-01 18:53:21 +0100

    Simplifies SelectMany

--- Monads/Maybe.cs
+++ Monads/Maybe.cs
@@ -27,18 +27,18 @@ namespace Monads {
     public static class Maybe {
         public static Maybe<T> ToMaybe<T>(this T t) => new Maybe<T>(t);
 
-        public static Maybe<TResult> Select<T, TResult>(this Maybe<T> source, Func<T, TResult> func) => source.HasValue
-            ? new Maybe<TResult>(func(source.Value))
+        public static Maybe<TResult> Select<T, TResult>(this Maybe<T> maybe, Func<T, TResult> func) => maybe.HasValue
+            ? new Maybe<TResult>(func(maybe.Value))
             : Maybe<TResult>.Nothing;
 
-        public static Maybe<TResult> SelectMany<T, TResult>(this Maybe<T> source,
-            Func<T, Maybe<TResult>> func) => source.HasValue ? func(source.Value) : Maybe<TResult>.Nothing;
+        public static Maybe<TResult> SelectMany<T, TResult>(this Maybe<T> maybe,
+            Func<T, Maybe<TResult>> func) => maybe.HasValue
+            ? func(maybe.Value)
+            : Maybe<TResult>.Nothing;
 
-        public static Maybe<TResult> SelectMany<T, T2, TResult>(this Maybe<T> source, Func<T, Maybe<T2>> func,
-            Func<T, T2, TResult> func2) {
-            if (!source.HasValue) return Maybe<TResult>.Nothing;
-            var maybe = func(source.Value);
-            return maybe.HasValue ? new Maybe<TResult>(func2(source.Value, maybe.Value)) : Maybe<TResult>.Nothing;
-        }
+        public static Maybe<TResult> SelectMany<T, T2, TResult>(this Maybe<T> maybe, Func<T, Maybe<T2>> func,
+            Func<T, T2, TResult> func2) => maybe.HasValue
+            ? SelectMany(func(maybe.Value), x => new Maybe<TResult>(func2(maybe.Value, x)))
+            : Maybe<TResult>.Nothing;
     }
 }
